a97788032a24fd86d08e9a8d03b90c5e086fd942
Pierdniki <kotrabatowy@gmail.com>
2019-04-16 13:06:33 +0200

    sidebar the best in the whole world by Jacob and his friend

--- game.py
+++ game.py
@@ -7,8 +7,8 @@ BLACK = 1
 WHITE = -1
 
 size = 600
-p1 = 0
-p2 = 0
+whitePoints = 0
+blackPoints = 0
 cellSize = size / GSIZE
 rad = cellSize * 0.4
 strokeWidth = 5
@@ -33,12 +33,23 @@ def paintCell(x, y, color):
 
 
 def sideBar():
+    countPoints()
     pygame.draw.rect(screen, (0, 255, 0), (size, 0, sideBarSize, size));
-    textsurface = myfont.render('current:', False, (0, 0, 0))
-    screen.blit(textsurface,(size, 10))
+    textsurface = myfont.render('current:', True, (0, 0, 0))
+    screen.blit(textsurface,(size+cellSize, cellSize))
+    
+    paintCell(9,2, (255,255,255))
+    whitesurface = myfont.render(str(whitePoints), True, (0, 0, 0))
+    screen.blit(whitesurface,(size + 95, cellSize*3))
+
+    paintCell(9,4, (0,0,0))
+    blacksurface = myfont.render(str(blackPoints), True, (0, 0, 0))
+    screen.blit(blacksurface,(size + 95, cellSize*5))
+
     if(player == 1): paintCell(15, 0, (255, 255, 255))
     else: paintCell(10, 0, (0, 0, 0))
     pygame.display.flip()
+    
 
 def init():
     pygame.draw.rect(screen, (255, 255, 255), (0, 0, size, size));
@@ -62,6 +73,19 @@ def move(x, y):
     if(table[x][y] == 0):
         table[x][y] = player
         player *= -1;
+
+
+def countPoints():
+    global whitePoints, blackPoints
+
+    whitePoints, blackPoints = 0, 0
+    for i in range (GSIZE):
+        for j in range (GSIZE):
+            if table[i][j]==BLACK:
+                blackPoints=blackPoints+1
+            elif table[i][j]==WHITE:
+                whitePoints=whitePoints+1
+
 
 
 init()
