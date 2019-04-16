752f5538320289104674edfe92290d22aa7291db
Jakub ćzyż <czyz.jakbu@op.pl>
2019-04-16 11:08:59 +0200

    blabla

--- game.py
+++ game.py
@@ -1,12 +1,44 @@
+import math
 import pygame
 from pygame import gfxdraw
 
 GSIZE = 8
+BLACK = 1
+WHITE = -1
+
 size = 600
+p1 = 0
+p2 = 0
 cellSize = size / GSIZE
 rad = cellSize * 0.4
 strokeWidth = 5
-screen = pygame.display.set_mode((size, size))
+sideBarSize = 400
+player = 1;
+
+screen = pygame.display.set_mode((size + sideBarSize, size))
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', int(sideBarSize*0.14))
+
+table = [[0 for _ in range(GSIZE)] for _ in range(GSIZE)]
+table[int(GSIZE/2)][int(GSIZE/2)] = 1
+table[int(GSIZE/2 - 1)][int(GSIZE/2 - 1)] = 1
+table[int(GSIZE/2 - 1)][int(GSIZE/2)] = -1
+table[int(GSIZE/2)][int(GSIZE/2 - 1)] = -1
+
+def paintCell(x, y, color):
+    pygame.gfxdraw.filled_circle(screen, int((x + 0.5) * cellSize), int((y + 0.5) * cellSize), int(rad), (0, 0, 0))
+    pygame.gfxdraw.filled_circle(screen, int((x + 0.5) * cellSize), int((y + 0.5) * cellSize), int(rad - strokeWidth), color)
+    pygame.gfxdraw.aacircle(screen, int((x + 0.5) * cellSize), int((y + 0.5) * cellSize), int(rad), (0, 0, 0))
+    pygame.gfxdraw.aacircle(screen, int((x + 0.5) * cellSize), int((y + 0.5) * cellSize), int(rad - strokeWidth), color)
+
+
+def sideBar():
+    pygame.draw.rect(screen, (0, 255, 0), (size, 0, sideBarSize, size));
+    textsurface = myfont.render('current:', False, (0, 0, 0))
+    screen.blit(textsurface,(size, 10))
+    if(player == 1): paintCell(15, 0, (255, 255, 255))
+    else: paintCell(10, 0, (0, 0, 0))
+    pygame.display.flip()
 
 def init():
     pygame.draw.rect(screen, (255, 255, 255), (0, 0, size, size));
@@ -19,24 +51,33 @@ def paint():
     for i in range(GSIZE):
         for j in range(GSIZE):
             if(table[i][j] == 1):
-                pygame.gfxdraw.filled_circle(screen, int((i + 0.5) * cellSize), int((j + 0.5) * cellSize), int(rad), (0, 0, 0))
-                pygame.gfxdraw.aacircle(screen, int((i + 0.5) * cellSize), int((j + 0.5) * cellSize), int(rad), (0, 0, 0))
+                paintCell(i, j, (0, 0, 0))
             elif(table[i][j] == -1):
-                pygame.gfxdraw.filled_circle(screen, int((i + 0.5) * cellSize), int((j + 0.5) * cellSize), int(rad), (0, 0, 0))
-                pygame.gfxdraw.filled_circle(screen, int((i + 0.5) * cellSize), int((j + 0.5) * cellSize), int(rad - strokeWidth), (255, 255, 255))
-                pygame.gfxdraw.aacircle(screen, int((i + 0.5) * cellSize), int((j + 0.5) * cellSize), int(rad), (0, 0, 0))
-                pygame.gfxdraw.aacircle(screen, int((i + 0.5) * cellSize), int((j + 0.5) * cellSize), int(rad - strokeWidth), (255, 255, 255))
+                paintCell(i, j, (255, 255, 255))
 
     pygame.display.flip()
 
-table = [[0 for _ in range(GSIZE)] for _ in range(GSIZE)]
+def move(x, y):
+    global player
+    if(table[x][y] == 0):
+        table[x][y] = player
+        player *= -1;
+
 
 init()
 paint();
-
+move(2, 2);
+paint();
+sideBar();
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            x = math.floor(x / cellSize)
+            y = math.floor(y / cellSize)
+
+            print(str(x)+" "+str(y));
